7e1c3076650f70c018409b0ecffcc853bb2a0701
Gabriel <gabrielsilva291996@gmail.com>
2022-01-05 15:17:15 -0300

    Backend Rota e Operação para Upload

--- Back-End/server.js
+++ Back-End/server.js
@@ -11,12 +11,17 @@ dotenv.config()
 import conectarDB from './config/db.js'
 conectarDB()
 
+//Nodejs module
+import path from 'path'
+
 //Rotas
 import postagensRoutes from './routes/postagensRoutes.js'
 import usuarioRoutes from './routes/usuariosRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 app.use('/api/postagens', postagensRoutes)
 app.use('/api/users', usuarioRoutes)
+app.use('/api/upload', uploadRoutes)
 
 //Middlewares de erro
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
@@ -28,6 +33,10 @@ app.use(errorHandler)
 app.get('/', (req, res)=>{
     res.send('API está funcionando...')
 })
+
+//Deixar a pasta estatica no express
+const __dirname = path.resolve()
+app.use('Front-End/public/images', express.static(path.join(__dirname, 'Front-End/public/images')))
 
 
 const PORTA = process.env.PORTA || 5000
